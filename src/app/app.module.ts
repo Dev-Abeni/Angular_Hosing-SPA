3652ae83ea97536b44b60786fd21dedb4a72a542
Abenezer Mulugeta <abenezermulugeta31@gmail.com>
2021-08-17 17:26:12 +0300

    Prevent the compiler following strict intialization of variables in tsconfig.json  & start working with template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
   ],
--- src/app/property/property-add/property-add.component.ts
+++ src/app/property/property-add/property-add.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,15 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./property-add.component.css']
 })
 export class PropertyAddComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm: NgForm;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
-  onBack()
-  {
-    this.router.navigate(['/']);
+  // Back to a specific component when the user clicks a back link
+  // onBack()
+  // {
+  //   this.router.navigate(['/']);
+  // }
+
+  onSubmit(Form: NgForm){
+    console.log("Form submitted");
+    console.log(this.addPropertyForm)
   }
 
 }
